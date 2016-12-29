4468d081cc112435a7af2bdf9805e121fb9be728
Sombo <psycoken@ymail.com>
2016-12-29 19:49:02 +0200

    put menu in func

--- TerminalTube.py
+++ TerminalTube.py
@@ -105,113 +105,148 @@ def play(url):
     a = subprocess.Popen(["mpv","--no-terminal","--autofit=33%","--geometry=99%:1%",url])
     return a
 
-def download(url,song_num):
-    location = "~/Developer/temp_downloads/"+songs_list['song'+str(song_num)]['name']+".%(ext)s"
+def download(url,song_num,media_list):
+    location = "~/"+media_list['song'+str(song_num)]['name']+".%(ext)s"
+    print (songs_list)
     call(["youtube-dl",'--output',location,"--extract-audio","--audio-format","mp3",url])
     raw_input("Success, Press Any key to go back...")
 
-def menu(media_type):
-    max_items = 0
+# def menu(media_type):
+    # max_items = 0
 
-    if media_type == 0:
-        print_Media_Title("Songs List")
-        media_list = songs_list
-        max_items = 15
+    # if media_type == 0:
+    #     print_Media_Title("Songs List")
+    #     media_list = songs_list
+    #     max_items = 15
     
-    else: 
-        print_Media_Title("Playlists List")
-        media_list = playlist_dict
-        max_items = len(media_list)
+    # else: 
+    #     print_Media_Title("Playlists List")
+    #     media_list = playlist_dict
+    #     max_items = len(media_list)
 
 
-    for i in range(1,max_items):
-        print "[%d]  %s" % (i,media_list['song'+str(i)]['name'])
-    print "\nPress 0 to return back."
+    # for i in range(1,max_items):
+    #     print "[%d]  %s" % (i,media_list['song'+str(i)]['name'])
+    # print "\nPress 0 to return back."
 
-    user_select = input("\nChoose Item : \n")
-    if user_select is 0:
-        return
+    # user_select = input("\nChoose Item : \n")
+    # if user_select is 0:
+    #     return
     
-    url = 'https://www.youtube.com' + media_list['song'+str(user_select)]['url']
+    # url = 'https://www.youtube.com' + media_list['song'+str(user_select)]['url']
 
-    # Mpv process object
-    proc = play(url)
+    # # Mpv process object
+    # proc = play(url)
     
-    # Sub Menu
-    while True:
-        print_Title()
-        print "Playing " + songs_list['song'+str(user_select)]['name']
+    # # Sub Menu
+    # while True:
+    #     print_Title()
+    #     print "Playing " + songs_list['song'+str(user_select)]['name']
 
-        action = raw_input("\n[S]top [D]ownload [B]ack\n\n?>")
+    #     action = raw_input("\n[S]top [D]ownload [B]ack\n\n?>")
         
-        if action is "d":
-            print "[*]Downloading " + songs_list['song'+str(user_select)]['name']
-            download(url,user_select)
-        elif action is "s":
-            proc.kill()
-            return
-        elif action is "b":
-            return
+    #     if action is "d":
+    #         print "[*]Downloading " + songs_list['song'+str(user_select)]['name']
+    #         download(url,user_select)
+    #     elif action is "s":
+    #         proc.kill()
+    #         return
+    #     elif action is "b":
+    #         return
 
-        else:
-            pass
+    #     else:
+    #         pass
+def main_menu():
+    max_items = 0
+    try:
+        while True:
+            print_Title()
+            user_in = raw_input("[1]:search\n[0]:exit\n\n?>")
 
+            if user_in is str(1):
+                print_Title()
+                songs_list ,playlist_dict = search_song()
+                break
+            elif user_in is str(0):
+                print_Title()
+                print "GoodBye..."
+                sys.exit()
+            else:
+                pass
 
+        while True:
+            try:
+                print_Title()
+                print "Results for '%s'\n" % search_query
+                msg = "[1]:Songs (%d)\n[2]:Playlists (%d)\n[3]:New_search\n\n[0]:Exit\n\n?>" % (len(songs_list),len(playlist_dict))
+                user_in = input(msg)
+                if user_in is 1:
+                    print_Media_Title("Songs List")
+                    media_list = songs_list
+                    max_items = 15
+                    pass
+                elif user_in is 2:
+                    print_Media_Title("Playlists List")
+                    media_list = playlist_dict
+                    max_items = len(media_list)
+                    pass
+                elif user_in is 3: #new search
+                    songs_list ,playlist_dict = search_song()
+                    pass
+                elif user_in is 0: #exit
+                    print "GoodBye..."
+                    sys.exit()
+        
+                for i in range(1,max_items):
+                    print "[%d]  %s" % (i,media_list['song'+str(i)]['name'])
+                print "\nPress 0 to return back."
+
+                user_select = input("\nChoose Item : \n")
+                if user_select is 0:
+                    return
+
+                url = 'https://www.youtube.com' + media_list['song'+str(user_select)]['url']
+
+                # Mpv process object
+                proc = play(url)
+    
+                # Sub Menu
+                while True:
+                    print_Title()
+                    print "Playing " + songs_list['song'+str(user_select)]['name']
+
+                    action = raw_input("\n[S]top [D]ownload [B]ack\n\n?>")
+        
+                    if action is "d":
+                        print "[*]Downloading " + songs_list['song'+str(user_select)]['name']
+                        download(url,user_select,media_list)
+                        pass
+                    elif action is "s":
+                        proc.kill()
+                        break
+                    elif action is "b":
+                        break
+
+                    else:
+                        pass
+
+            except NameError,e:
+                print e
+                input()
+                pass
+    except KeyboardInterrupt:
+        print "You Pressed Ctrl+C"
+        sys.exit()
+    except NameError,e:
+        print "Please use ' ' ."
 
 songs_list = {}
 playlist_dict = {}
 search_query =""
+max_items = 0
 
-
-
-
-try:
-    while True:
-        print_Title()
-        user_in = raw_input("[1]:search\n[0]:exit\n\n?>")
-
-        if user_in is str(1):
-            print_Title()
-            songs_list ,playlist_dict = search_song()
-            break
-        elif user_in is str(0):
-            print_Title()
-            print "GoodBye..."
-            sys.exit()
-        else:
-            pass
-
-    while True:
-        try:
-            print_Title()
-            print "Results for '%s'\n" % search_query
-            msg = "[1]:Songs (%d)\n[2]:Playlists (%d)\n[3]:New_search\n\n[0]:Exit\n\n?>" % (len(songs_list),len(playlist_dict))
-            user_in = input(msg)
-            if user_in is 1:
-                menu(0)
-                pass
-            elif user_in is 2:
-                menu(1)
-                pass
-            elif user_in is 3: #new search
-                songs_list ,playlist_dict = search_song()
-                pass
-            elif user_in is 0: #exit
-                print "GoodBye..."
-                sys.exit()
-        except NameError,e:
-            print e
-            input()
-            pass
-except KeyboardInterrupt:
-    print "You Pressed Ctrl+C"
-    sys.exit()
-except NameError,e:
-    print "Please use ' ' ."
-
-
-
-
+while True:
+    main_menu()
 
 
 
